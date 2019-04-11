f6c2567cbd86541f50510c20817ca8d52a90c875
Avinash <avinash8142@gmail.com>
2019-04-11 16:43:49 +0530

    changed pom and controller

--- spring-boot-git-test/src/main/java/com/web/springbootgittest/SpringBootGitTestApplication.java
+++ spring-boot-git-test/src/main/java/com/web/springbootgittest/SpringBootGitTestApplication.java
@@ -2,9 +2,10 @@ package com.web.springbootgittest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootGitTestApplication {
+public class SpringBootGitTestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGitTestApplication.class, args);
